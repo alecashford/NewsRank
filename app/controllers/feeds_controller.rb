f978ab5af3e6b187572bbfa84379cd4d745f5410
Kris Shanks <shanks@sonic.net>
2014-07-29 13:04:32 -0700

    tests for feeds controller

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -22,37 +22,13 @@ class FeedsController < ApplicationController
           feed.topics = result['results'][0]['deliciousTags'].join(',')
         end
       feed.save
-  end
-    associate_user(feed)
+    end
+
+    current_user.feeds << feed
     helper = FeedlyHelper.new(feed.feedly_feed_id)
     helper.add_to_db
-    redirect_to '/'
+    head :success
   end
-
-  #Currently this leaves the following fields blank:
-  # number of subscribers, description, topics
-  # def from_opml
-  #   f = File.open(params[:file])
-  #   doc = Nokogiri::XML(f)
-  #   doc.xpath("//outline").each do |x|
-  #     unless x['xmlUrl']==nil
-  #       feed = Feed.new
-  #         feed.url = x['xmlUrl']
-  #         feed.name = x['title']
-  #         feed.feedly_feed_id = "feed/#{x['xmlUrl']}"
-  #         feed.save
-  #         #assumes if not saved, feed already exists
-  #       if !feed.save
-  #         feed = Feed.find_by_feedly_feed_id(feed.feedly_feed_id)
-  #       end
-  #       current_user.feeds << feed
-  #       helper = FeedlyHelper.new(feed.feedly_feed_id)
-  #       helper.dump
-  #     end
-  #   end
-  # end
-
-
 
   def search
     search = FeedlyFinder.new(URI::encode(params[:url]))
