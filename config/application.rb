ff9190fcc3e3dee5906a1544394a32083348d2c6
Andrew Koines <akoines@zoho.com>
2014-07-30 16:20:30 -0700

    precompile false

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,6 @@ module NewsRank
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = false
   end
 end
