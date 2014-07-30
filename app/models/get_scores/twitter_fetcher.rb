db472edb03a99f58cd0b45136598b714e3c7d198
Kris Shanks <shanks@sonic.net>
2014-07-30 11:51:50 -0700

    tests for twitter fetcher and reddit fetcher

--- app/models/get_scores/twitter_fetcher.rb
+++ app/models/get_scores/twitter_fetcher.rb
@@ -5,11 +5,14 @@ module GetScores
     end
 
     def count
+      if fetch
       fetch["count"]
     end
 
     def fetch
       tw_response = HTTParty.get("http://cdn.api.twitter.com/1/urls/count.json?url=#{@url}")
+    rescue
+      nil
     end
   end
 end
